881da55d431d859af2bd99b111bf047d0f2be024
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-11 00:05:01 +0900

    HelloController
    기존 hello()를 복사 및 수정하여 위 URL-1과 같은 요청을 처리하는helloAndPerform1() 메소드를 정의함
     @RequestParam을 이용하여 요청 파라미터 값들을 추출
     HelloService#getGreeting() 및 HelloService#makePerformance()를 호출하여
    인사말과 공연 실행 결과를 구함
     ModelAndView 객체를 통해 “perform” view에 전달하여 출력시킴
     위 URL-2와 같은 요청을 처리하는 helloAndPerform2() 메소드를 정의함
     @PathVariable을 이용하여 요청 파라미터 값들을 추출
     helloAndPerform1()과 동일한 코드를 작성하거나, helloAndPerform1()을 호출

--- HelloSpringIdol3/src/main/java/com/example/helloworld/web/HelloController.java
+++ HelloSpringIdol3/src/main/java/com/example/helloworld/web/HelloController.java
@@ -5,12 +5,15 @@ import java.util.Calendar;
 import com.example.helloworld.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.springidol.SpringIdol;
 
+@RequestMapping("/helloSpringIdol")
 @Controller
 public class HelloController {
 	
@@ -22,15 +25,46 @@ public class HelloController {
 	HelloService helloService;
 	
 	@RequestMapping("/hello.do")		// request handler method
-	public ModelAndView hello(			
+	public ModelAndView hello(
 		@RequestParam(value="name", required=false) String name) {
-		
+
 		String greeting = helloService.getGreeting();
 		if (name != null) greeting = greeting + name;
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("hello");
 		mav.addObject("greeting", greeting);
 		return mav;
+	}
+	@GetMapping("/perform")
+	public String helloAndPerform1(@RequestParam String id, @RequestParam String requester){
+		String greeting = helloService.getGreeting();
+		String performance=null;
+		if (id != null) {
+			greeting = greeting + id;
+			performance = helloService.makePerformance(id);
+		}
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("perform");
+		mav.addObject("greeting", greeting);
+		mav.addObject("performance",performance);
+		return "perform";
+	}
+
+	@GetMapping("/perform/{id}/requester/{requester}")
+	public String helloAndPerform2(@PathVariable String id, @PathVariable String requester){
+		String greeting = helloService.getGreeting();
+		String performance=null;
+		if (id != null) {
+			greeting = greeting + id;
+			performance = helloService.makePerformance(id);
+		}
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("perform");
+		mav.addObject("greeting", greeting);
+		mav.addObject("performance",performance);
+		return "perform";
 	}
 
 
