1c0ad3e627e886b9c983103bd26a09af9e9d2311
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-11 00:44:58 +0900

    PerformRequest
    공연자 ID와 요청자 이름을 전송받기 위한 Command 클래스 PerformRequest를 정의
     HelloController에 새로운 handler method
    helloAndPerformUsingComm(PerformRequest request, …)를 정의
     요청 URL: http://localhost:8080/helloSpringIdol/performUsingComm
     index.jsp에 공연자 ID와 요청자 이름을 입력받기 위한 HTML form을 정의

--- HelloSpringIdol3/src/main/java/com/example/helloworld/web/HelloController.java
+++ HelloSpringIdol3/src/main/java/com/example/helloworld/web/HelloController.java
@@ -2,13 +2,11 @@ package com.example.helloworld.web;
 
 import java.util.Calendar;
 
+import com.example.helloworld.domain.PerformRequest;
 import com.example.helloworld.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.springidol.SpringIdol;
@@ -43,7 +41,7 @@ public class HelloController {
 			greeting = greeting + id;
 			performance = helloService.makePerformance(id);
 		}
-
+		// ModelAndView 객체를 생성하여 뷰 이름과 모델 데이터를 설정
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("perform");
 		mav.addObject("greeting", greeting);
@@ -59,11 +57,31 @@ public class HelloController {
 			greeting = greeting + id;
 			performance = helloService.makePerformance(id);
 		}
-
+		// ModelAndView 객체를 생성하여 뷰 이름과 모델 데이터를 설정
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("perform");
 		mav.addObject("greeting", greeting);
 		mav.addObject("performance",performance);
+		return "perform";
+	}
+
+	@PostMapping("/performUsingComm")
+	public String helloAndPerformUsingComm(@RequestBody PerformRequest request){
+
+		// PerformRequest 객체를 통해 id와 requester를 전송받음
+		String id = request.getId();
+		String requester = request.getRequester();
+
+		String greeting = helloService.getGreeting() + " " + requester;
+		String performance = helloService.makePerformance(id);
+
+		// ModelAndView 객체를 생성하여 뷰 이름과 모델 데이터를 설정
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("perform");
+		mav.addObject("greeting", greeting);
+		mav.addObject("performance", performance);
+
+		// ModelAndView 객체를 반환
 		return "perform";
 	}
 
--- HelloSpringIdol3/src/main/webapp/index.jsp
+++ HelloSpringIdol3/src/main/webapp/index.jsp
@@ -6,5 +6,16 @@
 <body>
 	<div><a href="hello.do">http://.../helloSpringIdol-JavaConfig/hello.do</a></div><br>
 	<div><a href="hello.do?name=Chris">http://.../helloSpringIdol-JavaConfig/hello.do?name=Chris</a></div>
+	<form method="post" action="performUsingComm">
+    Performer:
+    <select name="id">
+    <option value="kenny">kenny</option>
+    <option value="hank">hank</option>
+    <option value="duke">duke</option>
+    <option value="lena">lena</option>
+    </select> <br>
+    Your name: <input type="text" name="requester" /><br/>
+    <input type="submit" value="전송"/>
+    </form>
 </body>
 </html>
