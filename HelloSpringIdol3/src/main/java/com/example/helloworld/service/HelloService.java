40664809082c8989898896b360ad1b0c85f881a3
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-10 22:50:30 +0900

    HelloService - (2)
     기존의 HelloController의 getGreeting() 메소드를 HelloService로 복사 및 수정

--- HelloSpringIdol3/src/main/java/com/example/helloworld/service/HelloService.java
+++ HelloSpringIdol3/src/main/java/com/example/helloworld/service/HelloService.java
@@ -4,6 +4,7 @@ import com.example.springidol.Performer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,5 +16,17 @@ public class HelloService {
     @Autowired
     public HelloService(Map<String, Performer> performers) {
         this.performers = performers;
+    }
+
+    public String getGreeting() {		// business method
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (hour >= 6 && hour <= 10) {
+            return "Good morning! ";
+        } else if (hour >= 12 && hour <= 15) {
+            return "Did you have lunch? ";
+        } else if (hour >= 18 && hour <= 24) {
+            return "Good evening! ";
+        }
+        return "Hello! ";
     }
 }
