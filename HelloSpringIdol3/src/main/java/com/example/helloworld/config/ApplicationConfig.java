68169adf849a13c8faf55a69259137954fba2492
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-11 00:11:59 +0900

    Annotation 기반 bean 설
    service 패키지를 bean
    scan 대상으로 추가함

--- HelloSpringIdol3/src/main/java/com/example/helloworld/config/ApplicationConfig.java
+++ HelloSpringIdol3/src/main/java/com/example/helloworld/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages={"com.example.springidol"
-							 /*, "com.example.helloworld.service" */ }) 
+							 , "com.example.helloworld.service"  })
 @PropertySource("classpath:spring/app.properties")
 public class ApplicationConfig {
 	/* @ComponentScan 미적용 시 아래와 같이 명시적 bean 설정 필요 */
